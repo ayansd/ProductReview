4b0c55bc1c7f830cf0bd3a0c44afcd4c28497239
SanjayThanigaivelu <sanjaymuthulakshmi@gmail.com>
2025-03-30 19:10:03 +0530

    Backend End Point

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://productreview-frontend.onrender.com'],
+    origin: ['http://localhost:3000', 'https://productreview01.netlify.app/'],
     credentials: true,
   })
 );
