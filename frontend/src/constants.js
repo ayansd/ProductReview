01eb52e2babdbe6958d6e397a9fa1a2fb58291d3
SanjayThanigaivelu <sanjaymuthulakshmi@gmail.com>
2025-03-30 17:59:07 +0530

    constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL = 'https://productreview.onrender.com';
-export const PRODUCTS_URL = '/api/products';
-export const USERS_URL = '/api/users';
-export const ORDERS_URL = '/api/orders';
-export const PAYPAL_URL = '/api/config/paypal';
+export const PRODUCTS_URL = `${BASE_URL}/api/products`;
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const ORDERS_URL = `${BASE_URL}/api/orders`;
+export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;
