c4d557c597f204eb1ba87b6252c5042bbfd4e0b2
SanjayThanigaivelu <sanjaymuthulakshmi@gmail.com>
2025-03-30 17:12:29 +0530

    Backend

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:3000', 
+  })
+);
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
