b2d8e55a7a5ff9216981d670735c46f78b94149f
SanjayThanigaivelu <sanjaymuthulakshmi@gmail.com>
2025-03-30 18:00:13 +0530

    merge

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -2,4 +2,4 @@ export const BASE_URL = 'https://productreview.onrender.com';
 export const PRODUCTS_URL = `${BASE_URL}/api/products`;
 export const USERS_URL = `${BASE_URL}/api/users`;
 export const ORDERS_URL = `${BASE_URL}/api/orders`;
-export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;
+export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`
